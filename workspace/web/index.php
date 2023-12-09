9c14f745b129a6e1a59dd687aa0d22b253324971
mprieto01 <90802311+mprieto01@users.noreply.github.com>
2023-12-09 13:50:08 -0500

    Init files

---
+++ workspace/web/index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo phpinfo();
+
+?>
+<html>
+
+<head>
+    <script src="JS/jquery-3.7.1.min.js"></script>
+
+    <script>
+    $(document).ready(function(){
+        alert('inside');
+    });
+    </script>
+</head>
+<body>
+
+</body>
+</html>    
